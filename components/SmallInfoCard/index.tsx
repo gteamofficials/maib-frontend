f85ffb846e315394af1f5cd07397739685240175
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-05-30 15:49:04 +0700

    refactor sm-info-card to use grid layout

--- components/SmallInfoCard/index.tsx
+++ components/SmallInfoCard/index.tsx
@@ -5,20 +5,20 @@ import sampleImg from "../../public/samples/3.jpg";
 const SmallInfoCard = () => {
   return (
     <article className={styles.SmallInfoCard}>
-      <Image
-        src={sampleImg}
-        alt="image of"
-        width={160}
-        height={104}
-        className={styles.media}
-      />
-      <div className={styles.content}>
-        <h3 className={styles.header}>Sharing Anak Yatim</h3>
-        <time className={styles.date}>Selasa | 24 Mei 2022</time>
-        <a href="#" className={styles.action}>
-          Lihat detail
-        </a>
-      </div>
+      <span className={styles.media}>
+        <Image
+          src={sampleImg}
+          alt="image of"
+          layout="fill"
+          objectFit="cover"
+          className={styles.media}
+        />
+      </span>
+      <h3 className={styles.header}>Sharing Anak Yatim</h3>
+      <time className={styles.date}>Selasa | 24 Mei 2022</time>
+      <a href="#" className={styles.action}>
+        Lihat detail
+      </a>
     </article>
   );
 };
