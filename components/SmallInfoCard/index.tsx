31c39637c1cac9cbe10705d891378f35bab8ed9f
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-05-30 16:30:14 +0700

    refactor small info card to use prop

--- components/SmallInfoCard/index.tsx
+++ components/SmallInfoCard/index.tsx
@@ -2,21 +2,31 @@ import Image from "next/image";
 import styles from "./SmallInfoCard.module.scss";
 import sampleImg from "../../public/samples/3.jpg";
 
-const SmallInfoCard = () => {
+type AppProps = {
+  media: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  date: string;
+  href: string;
+};
+
+const SmallInfoCard = ({ media, title, date, href }: AppProps) => {
   return (
     <article className={styles.SmallInfoCard}>
       <span className={styles.media}>
         <Image
-          src={sampleImg}
-          alt="image of"
+          src={media.src}
+          alt={media.alt}
           layout="fill"
           objectFit="cover"
           className={styles.media}
         />
       </span>
-      <h3 className={styles.header}>Sharing Anak Yatim</h3>
-      <time className={styles.date}>Selasa | 24 Mei 2022</time>
-      <a href="#" className={styles.action}>
+      <h3 className={styles.header}>{title}</h3>
+      <time className={styles.date}>{date}</time>
+      <a href={href} className={styles.action}>
         Lihat detail
       </a>
     </article>
