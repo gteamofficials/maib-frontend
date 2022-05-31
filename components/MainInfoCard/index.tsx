d7736fb823534a05278de813baef79a20ad931e6
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-05-31 18:03:53 +0700

    create main info card

---
+++ components/MainInfoCard/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./MainInfoCard.module.scss";
+
+type AppProps = {
+  media: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  date: string;
+  href: string;
+};
+
+const MainInfoCard = ({ media, title, date, href }: AppProps) => {
+  return (
+    <article className={styles.mainInfoCard}>
+      <span className={styles.media}>
+        <Image
+          className={styles.image}
+          alt={media.alt}
+          src={media.src}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </span>
+      <div className={styles.content}>
+        <h3 className={styles.header}>{title}</h3>
+        <time className={styles.date}>{date}</time>
+      </div>
+      <Link href={href}>
+        <a className={styles.action}>Try it now</a>
+      </Link>
+    </article>
+  );
+};
+
+export default MainInfoCard;
--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,5 @@
 import Button from "./button";
 import HijriDate from "./HijriDate";
+import MainInfoCard from "./MainInfoCard";
 
-export { Button, HijriDate };
+export { Button, HijriDate, MainInfoCard };
