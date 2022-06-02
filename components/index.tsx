6d56a2cf76ebad707e02b4ccaabea8063ec8a02d
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-02 11:55:57 +0700

    export main, secondary, and additional info card

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,13 @@
 import Button from "./button";
 import HijriDate from "./HijriDate";
 import MainInfoCard from "./MainInfoCard";
+import SecondaryInfoCard from "./SecondaryInfoCard";
+import AdditionalInfoCard from "./AdditionalInfoCard";
 
-export { Button, HijriDate, MainInfoCard };
+export {
+  Button,
+  HijriDate,
+  MainInfoCard,
+  SecondaryInfoCard,
+  AdditionalInfoCard,
+};
