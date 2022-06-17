f7264bb03d72c06d257dd5772478ed2498835f9e
andienn <andiennovika20@gmail.com>
2022-06-17 23:08:50 +0700

    delete infocard asset

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,44 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <BigInfoCard 
-          media={{src:"/infocard.png", alt:"biginfocard"}} 
-          title="Ramadhan berbagi 1443H Masjid Al-Ikhlas Bulusan"
-          date="18 Mei 2022"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id imperdiet morbi sed sit sed sed tincidunt eu. Facilisis at dolor, tincidunt consectetur massa sed augue sit pharetra. Semper odio viverra suscipit fringilla est scelerisque. Aliquam nisi, nulla libero, sapien, leo semper arcu, libero. Sed dictumst purus ut arcu fermentum, a arcu est tempor. Tellus scelerisque fermentum neque, dictum id diam consequat. Cursus vitae, odio urna purus. Dignissim consectetur imperdiet elit mi ut."
-          href="instagram.com"></BigInfoCard>
+      <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
       </main>
 
       <footer className={styles.footer}>
