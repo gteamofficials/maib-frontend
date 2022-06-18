d4a2e2c3e7ca29ca94f7ba7917a5f72a200935e2
andienn <andiennovika20@gmail.com>
2022-06-18 21:48:56 +0700

    add prettier

--- components/InfoMenuCard/index.tsx
+++ components/InfoMenuCard/index.tsx
@@ -2,30 +2,30 @@ import styles from "./InfoMenuCard.module.scss";
 import Image from "next/image";
 
 type AppProps = {
-    media: {
-        src: string;
-        alt: string;
-      };
-    title: string;
-    date: string;
-    content: string;
+  media: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  date: string;
+  content: string;
 };
 
 const InfoMenuCard = ({ media, title, date, content }: AppProps) => {
   return (
     <article className={styles.infoMenuCard}>
-        <span className={styles.media}>
-            <Image
-            src={media.src}
-            alt={media.alt}
-            layout="fill"
-            objectFit="cover"
-            className={styles.media}
-            />
-        </span>
-        <h3 className={styles.title}>{title}</h3>
-        <time className={styles.date}>{date}</time>
-        <p className={styles.content}>{content}</p>
+      <span className={styles.media}>
+        <Image
+          src={media.src}
+          alt={media.alt}
+          layout="fill"
+          objectFit="cover"
+          className={styles.media}
+        />
+      </span>
+      <h3 className={styles.title}>{title}</h3>
+      <time className={styles.date}>{date}</time>
+      <p className={styles.content}>{content}</p>
     </article>
   );
 };
