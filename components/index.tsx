ea9d500c8e30d97f3caaf3267738966f2596cbe9
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-20 00:28:36 +0700

    add button link

--- components/index.tsx
+++ components/index.tsx
@@ -6,6 +6,7 @@ import SecondaryInfoCard from "./SecondaryInfoCard";
 import AdditionalInfoCard from "./AdditionalInfoCard";
 import NavDropDown from "./NavDropDown";
 import CategoryInfoCard from "./CategoryInfoCard";
+import ButtonLink from "./ButtonLink";
 
 export {
   Button,
@@ -16,4 +17,5 @@ export {
   AdditionalInfoCard,
   NavDropDown,
   CategoryInfoCard,
+  ButtonLink,
 };
