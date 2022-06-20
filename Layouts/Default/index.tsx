1454beed6651ade8cec6419d9f7c964ba6fe2c57
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-20 23:50:41 +0700

    add defalut layout

---
+++ Layouts/Default/index.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import { useWindowScroll } from "react-use";
+import { Footer, Navbar, NavDropDown } from "../../components";
+import styles from "./Default.module.scss";
+
+const DefaultLayout = ({ children }: { children: ReactNode }) => {
+  const { y } = useWindowScroll();
+
+  return (
+    <>
+      <Head>
+        <title>Masjid Al-Ikhlas</title>
+        <meta name="description" content="Website Masjid Al-Ikhlas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar opaque={y > 0 ? true : false}>
+        <Navbar.Item active href="#">
+          Beranda
+        </Navbar.Item>
+        <Navbar.Item>
+          <NavDropDown title="Informasi">
+            <NavDropDown.Item href="visi-misi">Visi Misi</NavDropDown.Item>
+            <NavDropDown.Item href="sejarah">Sejarah</NavDropDown.Item>
+          </NavDropDown>
+        </Navbar.Item>
+        <Navbar.Item href="#">Tentang</Navbar.Item>
+        <Navbar.Item href="#">Kontak</Navbar.Item>
+      </Navbar>
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default DefaultLayout;
