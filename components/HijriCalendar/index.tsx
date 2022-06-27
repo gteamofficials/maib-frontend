a9cef191c164111a4dff37c5eb5812d21eaca947
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-27 22:33:10 +0700

    add start day logic for claendar

--- components/HijriCalendar/index.tsx
+++ components/HijriCalendar/index.tsx
@@ -1,10 +1,11 @@
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import cn from "classnames";
 import HijriDate from "../HijriDate";
 import styles from "./HijriCalendar.module.scss";
 
 const HijriCalendar = () => {
   const Day = ["Ahd", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"];
-  const startDay = "ahad";
+  const startDay: String = "Sab";
 
   return (
     <div className={styles.hijriCalendar}>
@@ -15,8 +16,21 @@ const HijriCalendar = () => {
             {day}
           </div>
         ))}
-        {[...Array(30)].map((_, j) => (
-          <HijriDate key={j}>{j + 1}</HijriDate>
+        {[...Array(30)].map((_, i) => (
+          <HijriDate
+            className={cn({
+              [styles.ahd]: i === 0 && startDay === "Ahd",
+              [styles.sen]: i === 0 && startDay === "Sen",
+              [styles.sel]: i === 0 && startDay === "Sel",
+              [styles.rab]: i === 0 && startDay === "Rab",
+              [styles.kam]: i === 0 && startDay === "Kam",
+              [styles.jum]: i === 0 && startDay === "Jum",
+              [styles.sab]: i === 0 && startDay === "Sab",
+            })}
+            key={i}
+          >
+            {i + 1}
+          </HijriDate>
         ))}
       </div>
       <div className={styles.monthChanger}>
