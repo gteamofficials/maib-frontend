3297ef912496f6430a4dbe343b5344894940a4b6
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-07-27 21:35:05 +0700

    add salah schedule api host

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["source.unsplash.com"],
+  },
   env: {
     strapiApiUrl: "https://maib-backend-staging.herokuapp.com/api/",
+    salahScheduleApiUrl: "https://api.myquran.com/v1/sholat/jadwal/1433"
   },
 };
 
