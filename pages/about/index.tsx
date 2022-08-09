7a8428f73a32fb1652339f26e2a21d198d460531
aryakpt <aryakrisna07@gmail.com>
2022-08-10 00:47:59 +0700

    about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import AboutServices from "../../services/abouts";
 import { AboutType } from "../../types/response";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
 import styles from "../../styles/about.module.scss";
 import { Autoplay, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,54 +16,48 @@ const About = ({ abouts }: { abouts: AboutType[] }) => {
   return (
     <div className={styles.about}>
       <section className={styles.vision}>
-        <div className={styles.container}>
-          <h1 className={styles.vision__title}>{vision[0].attributes.title}</h1>
-          <div className={styles.vision__body}>
-            <p className={styles.vision__body__description}>
-              {vision[0].attributes.description}
-            </p>
-            <div className={styles.vision__body__images}>
-              <span className={styles.vision__body__images__first}>
-                <Image
-                  src={vision[0].attributes.images.data[0].attributes.url}
-                  layout={`fill`}
-                />
-              </span>
-              <span className={styles.vision__body__images__second}>
-                <Image
-                  src={vision[0].attributes.images.data[1].attributes.url}
-                  layout={`fill`}
-                />
-              </span>
-            </div>
+        <h1 className={styles.vision__title}>{vision[0].attributes.title}</h1>
+        <div className={styles.vision__body}>
+          <p className={styles.vision__body__description}>
+            {vision[0].attributes.description}
+          </p>
+          <div className={styles.vision__body__images}>
+            <span className={styles.vision__body__images__first}>
+              <Image
+                src={vision[0].attributes.images.data[0].attributes.url}
+                layout={`fill`}
+              />
+            </span>
+            <span className={styles.vision__body__images__second}>
+              <Image
+                src={vision[0].attributes.images.data[1].attributes.url}
+                layout={`fill`}
+              />
+            </span>
           </div>
         </div>
       </section>
 
       <section className={styles.mission}>
-        <div className={styles.container}>
-          <h1 className={styles.mission__title}>
-            {mission[0].attributes.title}
-          </h1>
-          <div className={styles.mission__body}>
-            <div className={styles.mission__body__images}>
-              <span className={styles.mission__body__images__first}>
-                <Image
-                  src={mission[0].attributes.images.data[0].attributes.url}
-                  layout={`fill`}
-                />
-              </span>
-              <span className={styles.mission__body__images__second}>
-                <Image
-                  src={mission[0].attributes.images.data[1].attributes.url}
-                  layout={`fill`}
-                />
-              </span>
-            </div>
-            <p className={styles.mission__body__description}>
-              {mission[0].attributes.description}
-            </p>
+        <h1 className={styles.mission__title}>{mission[0].attributes.title}</h1>
+        <div className={styles.mission__body}>
+          <div className={styles.mission__body__images}>
+            <span className={styles.mission__body__images__first}>
+              <Image
+                src={mission[0].attributes.images.data[0].attributes.url}
+                layout={`fill`}
+              />
+            </span>
+            <span className={styles.mission__body__images__second}>
+              <Image
+                src={mission[0].attributes.images.data[1].attributes.url}
+                layout={`fill`}
+              />
+            </span>
           </div>
+          <p className={styles.mission__body__description}>
+            {mission[0].attributes.description}
+          </p>
         </div>
       </section>
 
@@ -70,19 +67,19 @@ const About = ({ abouts }: { abouts: AboutType[] }) => {
         </div>
         <div className={styles.history__body}>
           <Swiper
-            modules={[Autoplay, Navigation]}
-            grabCursor={true}
+            modules={[Navigation]}
+            // grabCursor={true}
             navigation={true}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
+            // autoplay={{
+            //   delay: 5000,
+            //   disableOnInteraction: false,
+            // }}
             centeredSlides={true}
           >
             {histories.map((history, i) => (
               <SwiperSlide key={i}>
                 <div
-                  className={styles.history__body_bg}
+                  className={styles.history__body__bg}
                   style={{
                     backgroundImage: `url(${history.attributes.images.data[0].attributes.url})`,
                   }}
