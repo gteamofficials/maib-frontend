ec1cc8c3310d9c64aa644f6f8ed615db913b5603
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-09 23:15:56 +0700

    fix biginfo card component to compatible with empty image

--- components/BigInfoCard/index.tsx
+++ components/BigInfoCard/index.tsx
@@ -1,29 +1,35 @@
-import styles from "./BigInfoCard.module.scss";
 import Image from "next/image";
-import Link from "next/link";
-import Button from "../button";
 import { CardWithBodyProps } from "../../types/components";
+import ButtonLink from "../ButtonLink";
+import styles from "./BigInfoCard.module.scss";
 
 const BigInfoCard = ({ date, href, title, media, body }: CardWithBodyProps) => {
   return (
     <article className={styles.bigInfoCard}>
       <span className={styles.media}>
-        <Image
-          src={media.src}
-          alt={media.alt}
-          layout="fill"
-          objectFit="cover"
-          className={styles.media}
-        />
+        {media.src ? (
+          <Image
+            src={media.src}
+            alt={media.alt}
+            layout="fill"
+            objectFit="cover"
+            className={styles.media}
+          />
+        ) : (
+          <div className={styles.media}></div>
+        )}
       </span>
       <h3 className={styles.title}>{title}</h3>
       <time className={styles.date}>{date}</time>
       <p className={styles.content}>{body}</p>
-      <Link href={href}>
-        <Button size="medium" variant="primary" className={styles.link}>
-          Selengkapnya
-        </Button>
-      </Link>
+      <ButtonLink
+        size="medium"
+        variant="primary"
+        href={href}
+        className={styles.link}
+      >
+        Selangkapnya
+      </ButtonLink>
     </article>
   );
 };
