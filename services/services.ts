1d1458326d958b0e12c4428d980135f1443367c0
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-10 13:18:49 +0700

    prettier write

--- services/services.ts
+++ services/services.ts
@@ -2,7 +2,9 @@ import { ServiceType } from "../types/response";
 import { strapiApi } from "./api";
 
 const GetAll = async () => {
-  const res = await strapiApi.get(`services?populate=%2A&sort=createdAt%3Adesc&pagination[limit]=4`);
+  const res = await strapiApi.get(
+    `services?populate=%2A&sort=createdAt%3Adesc&pagination[limit]=4`
+  );
   const response: ServiceType[] = res.data.data;
   return response;
 };
