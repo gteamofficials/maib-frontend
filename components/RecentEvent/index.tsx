f9df09d2844fcac61a1be40ca2e756754f2b2bf2
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-12 19:32:11 +0700

    fix recent event link

--- components/RecentEvent/index.tsx
+++ components/RecentEvent/index.tsx
@@ -33,7 +33,7 @@ const RecentEvent = ({
       <h2 className={styles.title}>{title}</h2>
       <p className={styles.content}>{body}</p>
       <Link href={href}>
-        <a href="href" className={styles.action}>
+        <a className={styles.action}>
           Lihat Detail
         </a>
       </Link>
