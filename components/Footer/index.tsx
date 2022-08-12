f4ae60f82446d4b688e6541ba9b1d1c8d7ce3ee7
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-08-12 17:32:08 +0700

    fix footer layout on bigger screen

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
-import { ReactNode } from "react";
 import logo from "../../public/Logo.png";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <ul className={styles.list}>
-        <li>Made with ❤️ by G-Team &copy; {new Date().getFullYear()}</li>
-        <li>
-          <ul className={styles.nestedList}>
-            <li>Layanan Kami</li>
-            <li>Berita</li>
-            <li>Artikel</li>
-          </ul>
-        </li>
-        <li>
-          <div className={styles.logo}>
-            <Image src={logo} alt="logo" width="60" height="40" />
-            <p>Masjid Al-Ikhlas Bulusan</p>
-          </div>
-        </li>
-      </ul>
+      <nav className={styles.content}>
+        <ul className={styles.list}>
+          <li>Made with ❤️ by G-Team &copy; {new Date().getFullYear()}</li>
+          <li>
+            <ul className={styles.nestedList}>
+              <li>Layanan Kami</li>
+              <li>Berita</li>
+              <li>Artikel</li>
+            </ul>
+          </li>
+          <li>
+            <div className={styles.logo}>
+              <Image src={logo} alt="logo" width="60" height="40" />
+              <p>Masjid Al-Ikhlas Bulusan</p>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
